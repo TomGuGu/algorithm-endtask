6a958363bcc093a3ae15206ce190e2834c68f0ed
Volta <volta@stu.cqut.edu.cn>
2023-12-20 18:08:50 +0800

    Complete G3Q1

--- src/g1/Q6MajSoul.java
+++ src/g1/Q6MajSoul.java
@@ -8,39 +8,33 @@ import java.util.Scanner;
 public class Q6MajSoul {
 
   public static void main(String[] args) {
-    // 初始化剩余的牌
-    int[] brandsLeftCounter = new int[10];
-    for (int brand = 1; brand <= 9; brand++) {
-      brandsLeftCounter[brand] = 4;
-    }
-
     // 输入你有的牌
     Scanner scanner = new Scanner(System.in);
     int[] brandsYouHaveCounter = new int[10];
     for (int i = 1; i <= 13; i++) {
       int brand = scanner.nextInt();
       brandsYouHaveCounter[brand]++;
-      brandsLeftCounter[brand]--;
     }
 
     // 看看还能用哪些牌
     List<Integer> brandsCanUse = new ArrayList<>();
-    for (int brand = 0; brand < brandsLeftCounter.length; brand++) {
-      if (brandsLeftCounter[brand] > 0) {
+    for (int brand = 1; brand <= 9; brand++) {
+      if (brandsYouHaveCounter[brand] < 4) {
         brandsCanUse.add(brand);
       }
     }
 
-    // 把能用的牌依次加进来试试，如果能胡，就把这张牌加入结果集
+    // 把能用的牌依次加进来试试
     List<Integer> result = new ArrayList<>();
-    for (Integer brand : brandsCanUse) {
+    for (Integer brandAdded : brandsCanUse) {
       // 加进一张牌，看看能不能胡
-      brandsYouHaveCounter[brand]++;
+      brandsYouHaveCounter[brandAdded]++;
+      // 如果能胡，就把这张牌加入结果集
       if (isHu(brandsYouHaveCounter)) {
-        result.add(brand);
+        result.add(brandAdded);
       }
       // 拿走这张牌，进行下次尝试
-      brandsYouHaveCounter[brand]--;
+      brandsYouHaveCounter[brandAdded]--;
     }
 
     // 输出结果
@@ -58,9 +52,10 @@ public class Q6MajSoul {
   }
 
   private static boolean pickOutHeadBrands(int[] brandsCount) {
-    // 选出所有的雀头
+    // 选出所有的候选雀头
     List<Integer> headBrands = new ArrayList<>();
     for (int brand = 1; brand <= 9; brand++) {
+      // 如果该牌有两张以上，就可以作为雀头
       if (brandsCount[brand] >= 2) {
         headBrands.add(brand);
       }
@@ -77,12 +72,14 @@ public class Q6MajSoul {
     return false;
   }
 
+  // TODO: 记录牌的数量，最后判断的时候可以不用每次都遍历一遍
   private static boolean pickOutOtherBrands(int[] brandsCount) {
     for (int brand = 1; brand <= 9; brand++) {
       if (brandsCount[brand] == 0) {
         continue;
       }
 
+      // 刻子
       if (brandsCount[brand] >= 3) {
         brandsCount[brand] -= 3;
         if (pickOutOtherBrands(brandsCount)) {
