055b53ab5bea7390344bb471e91dd12690f3e79d
Volta <volta@stu.cqut.edu.cn>
2023-12-26 19:13:22 +0800

    Bugs fixed

--- src/g2/q5/Q5JobAssignment.java
+++ src/g2/q5/Q5JobAssignment.java
@@ -1,49 +1,53 @@
 package g2.q5;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Q5JobAssignment {
 
     public static void main(String[] args) {
-        int[][] costs = {
-                {3, 8, 2, 10},
-                {9, 7, 5, 3},
-                {1, 5, 4, 3},
-                {4, 5, 7, 9}
-        };
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入矩阵的大小：");
+        int length = scanner.nextInt();
+
+        int[][] costs = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                costs[i][j] = new Random().nextInt() % 10 + 10;
+            }
+        }
+
         System.out.println("任务分布如下：");
-        System.out.println(
-                """
-                        [3, 8, 2, 10]
-                        [9, 7, 5, 3]
-                        [1, 5, 4, 3]
-                        [4, 5, 7, 9]"""
-        );
+        for (int[] aCosts : costs) {
+            for (int cost : aCosts) {
+                System.out.printf("%2d ", cost);
+            }
+            System.out.println();
+        }
 
         BranchBoundary branchBoundary = new BranchBoundary(costs);
         branchBoundary.solve();
         int[] assignment = branchBoundary.getAssignment();
-        System.out.println("任务分配：");
+        System.out.println("分支限界法任务分配：");
         for (int i = 0; i < assignment.length; i++) {
-            System.out.println("人员" + (i + 1) + "分配任务" + (assignment[i]));
+            System.out.println("第" + (i + 1) + "任务分配给第" + (assignment[i] + 1) + "个人");
         }
-        System.out.println("该任务分配方式会使分配成本最小");
 
         BruteForce bruteForce = new BruteForce(costs);
         bruteForce.solve();
         assignment = bruteForce.getAssignment();
-        System.out.println("任务分配：");
+        System.out.println("蛮力法任务分配：");
         for (int i = 0; i < assignment.length; i++) {
-            System.out.println("任务" + (i + 1) + "交给员工" + (assignment[i] + 1));
+            System.out.println("第" + (i + 1) + "任务分配给第" + (assignment[i] + 1) + "个人");
         }
-        System.out.println("该任务分配方式会使分配成本最小");
 
         Hungarian hungarian = new Hungarian(costs);
         hungarian.solve();
         assignment = hungarian.getAssignment();
-        System.out.println("任务分配：");
+        System.out.println("匈牙利法任务分配：");
         for (int i = 0; i < assignment.length; i++) {
-            System.out.println("任务" + (i + 1) + "交给员工" + (assignment[i] + 1));
+            System.out.println("第" + (i + 1) + "任务分配给第" + (assignment[i] + 1) + "个人");
         }
-        System.out.println("该任务分配方式会使分配成本最小");
     }
 
 }
