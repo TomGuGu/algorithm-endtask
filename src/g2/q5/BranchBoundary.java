d649a8c76c4f2ae4883315a23576ad28b3388211
Volta <volta@stu.cqut.edu.cn>
2023-12-26 19:12:08 +0800

    Bugs fixed

--- src/g2/q5/BranchBoundary.java
+++ src/g2/q5/BranchBoundary.java
@@ -1,65 +1,98 @@
 package g2.q5;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class BranchBoundary {
     private static class Node implements Comparable<Node> {
-        int level; // 当前节点对应的人员编号
-        int cost; // 当前节点对应的成本
-        int[] assignment; // 当前节点对应的任务分配方案
+        // 当前已经分配到的人员编号
+        int no = -1;
 
-        public Node(int level, int cost, int[] assignment) {
-            this.level = level;
-            this.cost = cost;
-            this.assignment = assignment;
-        }
+        int lb;
+
+        int[] assignment;
 
         @Override
-        public int compareTo(Node other) {
-            return Integer.compare(cost, other.cost);
+        public int compareTo(Node o) {
+            return lb - o.lb;
+        }
+
+        public Node(int[] assignment) {
+            this.assignment = assignment;
         }
     }
 
-    private final int n; // 任务和人员的数量
-    private final int[][] personCosts; // 每个人员完成每项任务的成本
-    private final int[] assignment; // 最优分配方案
-    private int bestCost; // 最优成本
+    private final int[][] costs;
 
-    public BranchBoundary(int[][] personCosts) {
-        this.n = personCosts.length;
-        this.personCosts = personCosts;
-        this.assignment = new int[n];
-        this.bestCost = Integer.MAX_VALUE;
+    private int[] assignment;
+
+    private int minCost = Integer.MAX_VALUE;
+
+    private final int n;
+
+    private final PriorityQueue<Node> queue = new PriorityQueue<>();
+
+    public BranchBoundary(int[][] costs) {
+        this.costs = costs;
+        this.n = costs.length;
+
+        // 初始化根节点
+        int[] assignment = new int[n];
+        Arrays.fill(assignment, -1);
+        Node root = new Node(assignment);
+        root.lb = getLowerBound(root);
+        queue.add(root);
     }
 
     public void solve() {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        int[] initialAssignment = new int[n];
-        queue.offer(new Node(0, 0, initialAssignment));
-
         while (!queue.isEmpty()) {
             Node node = queue.poll();
-            int level = node.level;
-
-            if (level == n) {
-                if (node.cost < bestCost) {
-                    bestCost = node.cost;
-                    System.arraycopy(node.assignment, 0, assignment, 0, n);
-                }
-            } else {
-                for (int i = 0; i < n; i++) {
-                    if (node.assignment[i] == 0) {
-                        int[] newAssignment = node.assignment.clone();
-                        newAssignment[i] = level + 1;
-                        int newCost = node.cost + personCosts[level][i];
-                        queue.offer(new Node(level + 1, newCost, newAssignment));
-                    }
+            // 剪枝
+            if (node.lb >= minCost) {
+                continue;
+            }
+            // 已经分配到最后一个人，即找到一个可行解
+            if (node.no == n - 1) {
+                minCost = node.lb;
+                this.assignment = node.assignment;
+                continue;
+            }
+            // 扩展
+            for (int i = 0; i < n; i++) {
+                if (node.assignment[i] == -1) {
+                    Node child = new Node(Arrays.copyOf(node.assignment, n));
+                    child.no = node.no + 1;
+                    child.assignment[i] = child.no;
+                    child.lb = getLowerBound(child);
+                    queue.add(child);
                 }
             }
         }
     }
 
+    private int getLowerBound(Node node) {
+        int lb = 0;
+        // 计算已经分配的任务的成本
+        for (int i = 0; i < n; i++) {
+            if (node.assignment[i] != -1) {
+                lb += costs[node.assignment[i]][i];
+            }
+        }
+        // 计算未分配的任务的成本
+        for (int i = node.no + 1; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                if (node.assignment[j] == -1 && costs[i][j] < min) {
+                    min = costs[i][j];
+                }
+            }
+            lb += min;
+        }
+        return lb;
+    }
+
     public int[] getAssignment() {
         return assignment;
     }
+
 }
