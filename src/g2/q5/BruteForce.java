671167e7866e8ca426e8c4645c6cf0a19443d04e
Volta <volta@stu.cqut.edu.cn>
2023-12-26 19:11:51 +0800

    Bugs fixed

--- src/g2/q5/BruteForce.java
+++ src/g2/q5/BruteForce.java
@@ -3,45 +3,49 @@ package g2.q5;
 import java.util.Arrays;
 
 public class BruteForce {
-    private final int[][] costMatrix;
+    private final int[][] costs;
+
     private int[] assignment;
+
     private int minCost;
 
-    public BruteForce(int[][] costMatrix) {
-        this.costMatrix = costMatrix;
-        int n = costMatrix.length;
+    private final int n;
+
+    public BruteForce(int[][] costs) {
+        this.costs = costs;
+        this.n = costs.length;
+
         assignment = new int[n];
         Arrays.fill(assignment, -1);
         minCost = Integer.MAX_VALUE;
     }
 
     public void solve() {
-        int n = costMatrix.length;
         int[] permutation = new int[n];
         for (int i = 0; i < n; i++) {
             permutation[i] = i;
         }
-        permute(permutation, 0, n);
+        permute(permutation, 0);
     }
 
-    //    排列组合
-    private void permute(int[] permutation, int start, int n) {
+    // 排列组合
+    private void permute(int[] permutation, int start) {
         if (start == n - 1) {
             calculateCost(permutation);
         } else {
             for (int i = start; i < n; i++) {
                 swap(permutation, start, i);
-                permute(permutation, start + 1, n);
+                permute(permutation, start + 1);
                 swap(permutation, start, i);
             }
         }
     }
 
-    //计算成本
+    // 计算成本
     private void calculateCost(int[] permutation) {
         int cost = 0;
-        for (int i = 0; i < permutation.length; i++) {
-            cost += costMatrix[i][permutation[i]];
+        for (int i = 0; i < n; i++) {
+            cost += costs[i][permutation[i]];
         }
         if (cost < minCost) {
             minCost = cost;
